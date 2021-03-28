fc5b3aa17cad130cc3b822a1b7a7ff0f358fd49a
etoy22 <ethanleider@gmail.com>
2021-03-28 16:28:40 -0400

    Split server and client
    I did this because they shouldn't have access to each other so I used separate files to show this

--- Python Files/Server Files/server.py
+++ Python Files/Server Files/server.py
@@ -4,15 +4,14 @@ a link to the tutorial will be bellow
 https://www.techwithtim.net/tutorials/socket-programming/
 '''
 
+import sys
+# sys.path.append('./Helper_Functions')
+sys.path.append('./Path')
 import socket
 import threading
 import sqlite3
-import sys
 import json
 
-sys.path.append('./')
-sys.path.append('./Pages')
-sys.path.append('./Pages/Helper_Functions')
 
 from os import path
 
@@ -30,19 +29,26 @@ SERVER = socket.gethostbyname(socket.gethostname()) #Local Hosting
 ADDRESS = (SERVER,PORT)
 FORMAT = 'utf-8'
 
-#Currently Contains IP to give current user name
-account_tracker = {SERVER:'HOST'}
+''' 
+This will link the username that the user logs into with there IP.
+This means that the user cannot gain access to another account on that IP address while logged into a different account
+'''
+account_tracker = dict()
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDRESS)
 
 def send_client(message):
+    '''
+    This is used to send a message from the server back to the client
+    message - the message you want to send back to the client
+    '''
     msgOut = message.encode(FORMAT)
     msgOut_length = len(msgOut)
     send_length = str(msgOut_length).encode(FORMAT)
     send_length += b' ' * (HEADER-len(send_length))
-    return msgOut,send_length
-
+    con.send(send_length)
+    con.send(msgOut)
 
 def handle_client(con,adr):
     '''
@@ -66,28 +72,16 @@ def handle_client(con,adr):
                 users_list = get_usernames_list()
                 for name in users_list:
                     if username == name[0]:
-                        #  user already exist
-                        msgOut, send_length = send_client("already exist")
-                        con.send(send_length)
-                        con.send(msgOut)
+                        # user already exist
+                        send_client("already exist")
                     else:
                         try:
                             #CHECK #6 - Maybe we should make sure the password is secure
                             add_user(username, password)
-                            account_tracker[adr]=username
-                            msgOut = "success".encode(FORMAT)
-                            msgOut_length = len(msgOut)
-                            send_length = str(msgOut_length).encode(FORMAT)
-                            send_length += b' ' * (HEADER-len(send_length))
-                            con.send(send_length)
-                            con.send(msgOut)
+                            account_tracker[adr]=username #Ethan added this for later parts
+                            send_client("success")
                         except sqlite3.Error:
-                            msgOut = "error".encode(FORMAT)
-                            msgOut_length = len(msgOut)
-                            send_length = str(msgOut_length).encode(FORMAT)
-                            send_length += b' ' * (HEADER-len(send_length))
-                            con.send(send_length)
-                            con.send(msgOut)      
+                            send_client("error")
             #Case if someone attempts to login
             elif msg['CODE'] == ApplicationStates.LOGIN.value:
                 #Change earlier than this so that it actually requires the username and passwords to be correct
@@ -95,12 +89,13 @@ def handle_client(con,adr):
                 password = msg['SEC']
                 #  Checking if user exists
                 users_list = get_usernames_list()
-
                 for user in users_list:
                     if username == user[0]:
                         if password == get_master_pwd(username):
                             user_id = get_user_id(username)
+                            #CHECK #8 - What is adr[1]? adr refers to the computers adress
                             account_tracker[adr[1]] = user_id
+                            #CHECK #7 - makes sure
                             accessed = str(1)+"#"+str(user_id)
                             break
                         else:
@@ -108,27 +103,18 @@ def handle_client(con,adr):
                             break
                     else:
                         accessed = str(2)
-                    msgOut = accessed.encode(FORMAT)
-                    msgOut_length = len(msgOut)
-                    send_length = str(msgOut_length).encode(FORMAT)
-                    send_length += b' ' * (HEADER-len(send_length))
-                    con.send(send_length)
-                    con.send(msgOut)
+                    #CHECK #9 - Double check i didn't mess this up
+                    send_client(accessed)
             # send service list
             elif msg['CODE'] == ApplicationStates.GET_SERVICES.value:
                 user_id = account_tracker[adr[1]]
                 services_list = list_saved_services(user_id)
                 senddata = ""
+                #CHECK 1 - Might need to change how i data is sent back to the client
                 for service in services_list:
                     senddata += service[0]+"#"
                 senddata = senddata[:-1]
-                #CHECK 1
-                msgOut = senddata.encode(FORMAT)
-                msgOut_length = len(msgOut)
-                send_length = str(msgOut_length).encode(FORMAT)
-                send_length += b' ' * (HEADER-len(send_length))
-                con.send(send_length)
-                con.send(msgOut)
+                send_client(senddata)
             # add service to account
             elif msg['CODE'] == ApplicationStates.ADD_SERVICE.value:
                 service_name = msg['FIR']
@@ -145,12 +131,7 @@ def handle_client(con,adr):
                                 service_password, user_id)
                     accessed = str(1)
                 #CHECK 2 - What is being sent here
-                msgOut = accessed.encode(FORMAT)
-                msgOut_length = len(msgOut)
-                send_length = str(msgOut_length).encode(FORMAT)
-                send_length += b' ' * (HEADER-len(send_length))
-                con.send(send_length)
-                con.send(msgOut)     
+                send_client(accessed)  
             # check service
             elif msg['CODE'] == ApplicationStates.CHECK_SERVICE.value:
                 service_name = msg['FIR']
@@ -158,19 +139,14 @@ def handle_client(con,adr):
                 services_list = list_saved_services(user_id)
                 for service in services_list:
                     if service_name == service[0]:
+                        #CHECK #10 another case where we might need to change how data is sent
                         service_username, service_password = check_data_from_service(user_id, service_name)
                         accessed = str(1)+"#"+service_username+"#"+service_password
                         #CHECK 3 - Double check that this doesn't break out of loop 
                         break
                 else:
                     accessed = str(0)
-                msgOut = accessed.encode(FORMAT)
-                msgOut_length = len(msgOut)
-                send_length = str(msgOut_length).encode(FORMAT)
-                send_length += b' ' * (HEADER-len(send_length))
-                con.send(send_length)
-                con.send(msgOut)
-             # Update service
+                send_client(accessed)  
             # Update Username and Password for a service
             elif msg['CODE'] == ApplicationStates.UPDATE_SERVICE.value:
                 service_name = msg['FIR']
@@ -194,12 +170,7 @@ def handle_client(con,adr):
                             break
                 else:
                     service_exists = 0
-                msgOut = str(service_exists).encode(FORMAT)
-                msgOut_length = len(msgOut)
-                send_length = str(msgOut_length).encode(FORMAT)
-                send_length += b' ' * (HEADER-len(send_length))
-                con.send(send_length)
-                con.send(msgOut)
+                send_client(str(service_exists))   
             # Delete service
             elif msg['CODE'] == ApplicationStates.DELETE_SERVICE.value:
                 service_name = msg['FIR']
@@ -212,15 +183,10 @@ def handle_client(con,adr):
                         break
                 else:
                     service_exists = 0
-                msgOut = str(service_exists).encode(FORMAT)
-                msgOut_length = len(msgOut)
-                send_length = str(msgOut_length).encode(FORMAT)
-                send_length += b' ' * (HEADER-len(send_length))
-                con.send(send_length)
-                con.send(msgOut) 
+                send_client(str(service_exists))
             # Delete account
             elif msg['CODE'] == ApplicationStates.DELETE_ACCOUNT.value:
-                #CHECK 4 - maybe require a username password verification from server
+                #CHECK 4 - maybe require a username password verification
                 user_id = account_tracker[adr[1]]
                 delete_user(user_id)
             #Disconnect from server
@@ -230,6 +196,7 @@ def handle_client(con,adr):
                     print(account_tracker)
                 print(f"[{adr}] Has left the application")
                 connected = False
+                send_client("Disconected")  
     con.close()
 
 if __name__ == "__main__":
@@ -240,7 +207,3 @@ if __name__ == "__main__":
         thread = threading.Thread(target=handle_client, args=(con,adr))
         thread.start()
         print(f"[Accounts Connected] {threading.activeCount()-1}")
-
-        
-  
-    
