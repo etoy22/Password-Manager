056ae744588913936ca64cae52724539b496db5b
Robliuwei <44253880+Robliuwei@users.noreply.github.com>
2021-03-29 01:53:08 -0400

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,28 +1,108 @@
-import tkinter as tk
-from tkinter import filedialog, Text
-import os
-root = tk.Tk()
-canvas = tk.Canvas(root, height=700, width=700, bg="black")
-canvas.pack()
+import tkinter as tk              
+from tkinter import font as tkfont  
+from tkinter import *
+class SampleApp(tk.Tk):
 
-frame = tk.Frame(root,bg ="white")
-frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-def log_Window():
-    newWindow = tk.Toplevel(frame)
-    newWindow.title("log in page")
-    newWindow.geometry("400x400")
-def sign_Window():
-    newWindow = tk.Toplevel(frame)
-    newWindow.title("sign in page")
-    newWindow.geometry("400x400")
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        self.title_font = tkfont.Font(family='Helvetica', size=18, weight="bold", slant="italic")
+        container = tk.Frame(self)
+        container.pack(side = "top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight =1)
+        container.grid_columnconfigure(0, weight=1)
+  
 
+        self.frames = {}
+        for F in (StartPage, PageOne, PageTwo):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+        frame = self.frames[page_name]
+        frame.tkraise()
+
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Password Manager", font=controller.title_font)
+        label.pack(side=tk.TOP)
+
+        button1 = tk.Button(self, text="Log  in",
+                            command=lambda: controller.show_frame("PageOne"))
+        button2 = tk.Button(self, text="Sign in",
+                            command=lambda: controller.show_frame("PageTwo"))
+        button1.pack()
+        button2.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Log in page ", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack(side = BOTTOM)
+        canvas = tk.Canvas(self, height=500, width=600, bg="white")
+        canvas.pack()
+        label2 = tk.Label(self,text = "USER  ID: ",bg = "white", width = 8, font = ("arial", 10,"bold"))   
+        label2.place(x =10, y=130)
+        label3 = tk.Label(self,text = "Password: ",bg = "white", width = 8, font = ("arial", 10,"bold"))   
+        label3.place(x =10, y=180)
+        entry_1 = tk.Entry(self)
+        entry_1.place(x = 80, y =130)
+        entry_2 = tk.Entry(self)
+        entry_2.place(x = 80, y =180)
+ 
+
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        label = tk.Label(self, text="Sign in page ", font=controller.title_font)
+        label.pack(side="top", fill="x", pady=10)
+        button = tk.Button(self, text="Go to the start page",
+                           command=lambda: controller.show_frame("StartPage"))
+        button.pack(side = BOTTOM)
     
+        signup_buttom = tk.Button(self, text="Sign up",command=self.sign_up)
+        signup_buttom.pack(side = BOTTOM)
+        canvas = tk.Canvas(self, height=500, width=600, bg="white")
+        canvas.pack()
+    
+        id_string=tk.StringVar()
+        pwd_string=tk.StringVar()
+        label2 = tk.Label(self,text = "USER  ID: ",bg = "white", width = 8, font = ("arial", 10,"bold"))   
+        label2.place(x =10, y=130)
+        label3 = tk.Label(self,text = "Password: ",bg = "white", width = 8, font = ("arial", 10,"bold"))   
+        label3.place(x =10, y=180)
+        self.entry_1 = tk.Entry(self)
+        self.entry_1.place(x = 80, y =130)
+        self.id_string = self.entry_1.get()
+        self.entry_2 = tk.Entry(self,textvar = pwd_string)
+        self.entry_2.place(x = 80, y =180)
+        self.pwd_string = self.entry_2.get()
+        print(self.pwd_string)
+    def sign_up(self):
+        a = self.entry_1.get()
+        b = self.entry_2.get()
+        print("User Id is: "+str(a)+"\n"+ "password is: "+str(b))
 
 
-log_in = tk.Button(frame,text = "Log in", fg="white", bg="black", command = log_Window)
-log_in.pack(padx =10, pady=5, side= tk.BOTTOM)
 
-sign_in = tk.Button(frame,text = "sign in", fg="white", bg="black",command = sign_Window)
-sign_in.pack(padx =10, pady=5, side= tk.BOTTOM)
+if __name__ == "__main__":
 
-root.mainloop()
+    app = SampleApp()
+    app.mainloop()
+    
